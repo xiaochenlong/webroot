bbeba0dae35a7cb6c4c8b2be9e7dffda6d86eec9
ether007 <java_chn@163.com>
2015-12-27 00:13:12 +0800

    logback

--- src/main/java/org/ace/web/mvc/ctrls/SimpleController.java
+++ src/main/java/org/ace/web/mvc/ctrls/SimpleController.java
@@ -1,7 +1,40 @@
 package org.ace.web.mvc.ctrls;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 /**
  * Created by xiaochenlong on 2015/12/26.
  */
-public class SimpleController {
+
+@RestController
+@RequestMapping("/simple")
+public class  SimpleController{
+
+    static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
+
+
+    @RequestMapping(value = "/str")
+    public  String simple_str() {
+        logger.debug("debug....");
+        logger.info("info....");
+        logger.warn("warn....");
+        return "Hello world!";
+    }
+
+
+    @RequestMapping(value = {"/jo"},produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Model simple_json(Model model){
+        logger.warn("json....");
+        model.addAttribute("data",new Date());
+        model.addAttribute("version",2);
+        return  model;
+    }
+
 }
--- src/main/webapp/WEB-INF/jsp/simple.jsp
+++ src/main/webapp/WEB-INF/jsp/simple.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Administrator
-  Date: 2015/12/27
-  Time: 0:05
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-
+{date}
 </body>
 </html>
