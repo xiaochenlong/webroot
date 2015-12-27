0091e17c22993444dff129536a3103a032899ed7
ether007 <java_chn@163.com>
2015-12-27 18:30:39 +0800

    dao.未完成

---
+++ src/main/java/org/ace/web/dao/SimpleDao.java
@@ -0,0 +1,7 @@
+package org.ace.web.dao;
+
+/**
+ * Created by xiaochenlong on 2015/12/27.
+ */
+public interface SimpleDao {
+}
---
+++ src/main/java/org/ace/web/dao/impl/SimpleDaoImpl.java
@@ -0,0 +1,9 @@
+package org.ace.web.dao.impl;
+
+/**
+ * Created by xiaochenlong on 2015/12/27.
+ */
+
+public class SimpleDaoImpl {
+
+}
