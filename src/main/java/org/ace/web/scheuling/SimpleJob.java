91bee01d1d45d34479e148267e6768f988efccc3
chenlong <java_chn@163.com>
2016-02-15 15:15:40 +0800

    任务池

--- src/main/java/org/ace/web/scheuling/SimpleJob.java
+++ src/main/java/org/ace/web/scheuling/SimpleJob.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Component;
 
 /**
  * Created by xiaochenlong on 2015/12/27.
+ *
+ * fixedDelay 上次结束到下次开始间隔（如果执行时间大于此值则执行结束就执行下一次）
+ * fixedRate  上次开始到下次开始间隔
  */
 
-//@Component
+@Component
 public class SimpleJob {
 
     public SimpleJob() {
@@ -15,11 +18,22 @@ public class SimpleJob {
     }
 
     public void run(){
-        System.out.println("run..."+System.currentTimeMillis()/1000);
+       // System.out.println("run.00000000.."+System.currentTimeMillis()/1000);
     }
 
-    //@Scheduled(initialDelay=1000, fixedRate=5000)
+    @Scheduled(initialDelay=1000, fixedDelay = 3000)
     public void job(){
-        System.out.println("job..."+System.currentTimeMillis()/1000);
+        System.out.println("job.11111111.."+System.currentTimeMillis()/1000);
+        try {
+            Thread.sleep(1000);
+            System.out.println("vvvvvvvv");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(initialDelay=1000, fixedRate=5000)
+    public void job2(){
+        //System.out.println("job.2222222.."+System.currentTimeMillis()/1000);
     }
 }
